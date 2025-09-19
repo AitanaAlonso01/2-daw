7c8c2150f2dae8366f81417f779648d87bf77640
Aitana <aitana.alonso.lorenzo@gmail.com>
2025-09-19 09:50:37 +0200

    A - Ejercicios Clase 1 - Desarrollo Web en Entorno Cliente (DWEC)

---
+++ DWEC/U1/app2.js
@@ -0,0 +1,20 @@
+// 1. Mostrar tu nombre en una ventana emergente
+function mostrarNombre() {
+  alert("¡Hola, soy Aitana!");
+}
+
+// 2. Multiplicar dos números y mostrar el resultado en la consola
+function multiplicarNumeros() {
+  const num1 = 7;
+  const num2 = 5;
+  const resultado = num1 * num2;
+  console.log(`El resultado de ${num1} x ${num2} es ${resultado}`);
+}
+
+// 3. Juntar dos palabras y mostrarlas como una frase en la consola
+function crearFrase() {
+  const palabra1 = "JavaScript";
+  const palabra2 = "mola";
+  const frase = palabra1 + " " + palabra2 + " mucho.";
+  console.log("Frase: " + frase);
+}
