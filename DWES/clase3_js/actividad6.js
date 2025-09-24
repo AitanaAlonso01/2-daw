233d8a7c5e4c8bdf07405ef0abcdc71b87912b48
Aitana Clase <aitana.alonso.lorenzo@gmail.com>
2025-09-24 15:59:55 +0200

    A - Actividad 6 Extraer primer nombre de matriz de nombre y apellido HP - DWES

---
+++ DWES/clase3_js/actividad6.js
@@ -0,0 +1,14 @@
+// Define una variable llamada 'firstanmes' y asígnala al resultado de la asignación sobre la matriz existente, 'fullnames', de modo que 'firstnames' constenga solo los nombres de pila de los personajes de Harry Potter de la matriz 'fullnames'.
+
+const fullnames = [
+  { first: 'Albus', last: 'Dumbledore' },
+  { first: 'Harry', last: 'Potter' },
+  { first: 'Hermione', last: 'Granger' },
+  { first: 'Ron', last: 'Weasley' },
+  { first: 'Rubeus', last: 'Hagrid' },
+  { first: 'Minerva', last: 'McGonagall' },
+  { first: 'Severus', last: 'Snape' },
+]
+
+let firstnames = fullnames.map(item => item.first)
+console.log(firstnames)
