9886b73e8166b3478c3dd71d6524bd74d78b1f05
Aitana Clase <aitana.alonso.lorenzo@gmail.com>
2026-01-07 20:41:52 +0100

    U - Aprendiendo MongoDB Atlas - PI + DWES

--- DWES/SA6/clase2_AAA/models/comment.model.js
+++ DWES/SA6/clase2_AAA/models/comment.model.js
@@ -1,31 +1,31 @@
-const categorias = require("./categorias.model.json")
-const mongoose = require("mongoose")
+const categorias = require('./categorias.model.json')
+const mongoose = require('mongoose')
 
 const commentSchema = new mongoose.Schema({
-    //_id --> ID UUID (como la PK) - NO es un String. Se genera automáticamente
-    usuario:{
-        type:String,
-        required:true //NOT NULL SQL
-    },
-    opinion:{
-        type:String,
-        required:true //NOT NULL SQL        
-    },
-    categoria:{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:"categoria", //FK - necesario para populate
-        required:true        
-    },
-    valoracion:{
-        type:Number,
-        required:true,
-        min:0,
-        max:5
-    }
+  //_id --> ID UUID (como la PK) - NO es un String. Se genera automáticamente
+  usuario: {
+    type: String,
+    required: true, //NOT NULL SQL
+  },
+  opinion: {
+    type: String,
+    required: true, //NOT NULL SQL
+  },
+  categoria: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'categoria', //FK - necesario para populate
+    required: true,
+  },
+  valoracion: {
+    type: Number,
+    required: true,
+    min: 0,
+    max: 5,
+  },
 })
 
 //Creamos el MODELO a partir del ESQUEM MONGOOSE
-const comment = mongoose.model("comment", commentSchema)
+const comment = mongoose.model('comment', commentSchema)
 
 //Exportamos para poderlo usar en el SERVICE
 module.exports = comment
--- DWES/SA6/clase2_AAA/utils/mongodb.config.js
+++ DWES/SA6/clase2_AAA/utils/mongodb.config.js
@@ -1,6 +1,6 @@
-require("dotenv").config() //Acceder a .env (variables de entorno)
-const mongoose = require("mongoose") //npm i
+require('dotenv').config() //Acceder a .env (variables de entorno)
+const mongoose = require('mongoose') //npm i
 
-exports.conectarMongoDB = async() => {
-    return mongoose.connect(process.env.MONGODB_CONSTRING)
+exports.conectarMongoDB = async () => {
+  return mongoose.connect(process.env.MONGODB_ATLAS)
 }
